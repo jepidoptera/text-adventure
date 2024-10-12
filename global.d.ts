7889c83cc2511eede161fe3bf74d60d8515fb592
jepidoptera <jepidoptera@live.com>
2024-10-12 16:29:50 -0500

    make optionBox default_option optional

--- global.d.ts
+++ global.d.ts
@@ -3,7 +3,7 @@ declare global {
     function input(prompt?: string): Promise<string>;
     function clear(): void;
     function color(fg: string, bg?: string): void;
-    function optionBox(options: {title: string, options: string[], default_option: number}): Promise<number>;
+    function optionBox(options: {title: string, options: string[], default_option?: number}): Promise<number>;
     function getKey(options?: string[]): Promise<string>;
     function locate(x: number, y?: number)
     function pause(seconds: number): Promise<void>;
