0bb3e60b7f6ed36c5f0df962bb88d260bf38c6a7
jepidoptera <jepidoptera@live.com>
2024-10-26 23:09:59 -0500

    add some landmarks, including functional tree

--- src/scenarios/A2D/landmarks.ts
+++ src/scenarios/A2D/landmarks.ts
@@ -8,19 +8,110 @@ const landmarks: { [key: string]: (...args: any[]) => Landmark } = {
         if (Array.isArray(text)) {
             text = text.join('\n')
         }
+        // console.log(`Creating sign with text: ${text}`)
         return new Landmark({
             name: 'sign',
             description: 'A Sign',
             text: text
         }).action('read sign', async function () {
             color(gray)
-            print(this.text)
+            print(text)
         })
     },
     towering_tree(): Landmark {
         return new Landmark({
             name: 'tree',
             description: 'A Monstrous, Sky-Touching Tree',
+        }).action('climb tree', async function (player) {
+            color(black)
+            if (player.game.flags.turlin) {
+                print("Putting one hand above the other, you begin the familiar ascent.")
+                print("You climb... ")
+                for (let a = 1; a <= 3; a++) {
+                    await pause(1)
+                    print("and climb...")
+                }
+                pause(2)
+                print("You reach the top and look out. The world spreads before you.")
+                player.relocate(player.game.find_location("the top of the tree"))
+            }
+            print("You begin climbing the tree.")
+            await pause(1.8)
+            print("You reach the middle of the tree, the climbing gets harder.")
+            await pause(3)
+            print("You climb...")
+            await pause(1)
+            for (let a = 1; a <= 3; a++) {
+                print("And climb...")
+                await pause(1)
+            }
+            print()
+            print("You are now almost at the crown.  Some sort of platform like a tree house seems")
+            print("to have been build at the top.  You must be hundreds of feet in the air.  You")
+            print("get a sickening stomach-wrenching feeling looking downwards.")
+            print()
+            await pause(3)
+            print("Continue to climb? [y/n]")
+            if (await getKey(['y', 'n']) === 'y') {
+                print("Using your strength and agility you climb higher, maxing your physical")
+                print("abilities.")
+                await pause(2)
+                if (player.max_sp / 10 + player.strength + player.agility * 2 > 30) {
+                    // Made it
+                    print("You pull yourself at last onto the hard surface of the platorm")
+                    print("You lay breathing hard with only one though in your head:")
+                    print("''I am glad to be up here.'', a thought, until now, your mind new to be foreign.")
+                    await pause(10)
+                    print("You breathe heavily and sigh raising your head...")
+                    await pause(4)
+                    print("Your intestines feel as if they jumped into your throat and were strangling")
+                    print("you from within.  Standing before you is...")
+                    await pause(4)
+                    player.relocate(player.game.find_location("the top of the tree"))
+                } else {
+                    // Crash Caboom
+                    print("You are almost there...  You gasp")
+                    await pause(2)
+                    print("As you reach to grip the last branch, you feel")
+                    print("your other hand begin to slide off the branch it was holding.")
+                    print("Horified you realize that you are no longer on the tree... but in the air")
+                    print("and the ground is rushing up ever nearer.")
+                    await pause(6)
+                    print("You fall...")
+                    for (let a = 1; a <= 3; a++) {
+                        await pause(0.5)
+                        print("and fall...")
+                    }
+                    await pause(1)
+                    print("With a sickening jolt your world fades out...")
+                    await pause(2)
+                    color(black, black)
+                    clear()
+                    await pause(1.6)
+                    color(black, darkwhite)
+                    clear()
+                    print("You come to with a stinging sensation all over your body.  You are lucky to")
+                    print("be alive.")
+                    player.hp = 1
+                    player.sp = 1
+                }
+            } else {
+                print("You decide that you have gone far enough and begin the descent.")
+                await pause(2)
+                print("You reach the ground safely.")
+            }
+        })
+    },
+    treehouse_platform(): Landmark {
+        return new Landmark({
+            name: 'treehouse platform',
+            description: 'A Treehouse Platform',
+        }).action('climb down', async function (player) {
+            print("You begin your descent...")
+            await pause(2)
+            print("You continue to climb downwards...")
+            await pause(3)
+            print("After an exhausting climb you feel grateful to be on the ground again!")
         })
     },
     mixing_pot(): Landmark {
@@ -58,16 +149,18 @@ const landmarks: { [key: string]: (...args: any[]) => Landmark } = {
             await pause(1); print(".", 1);
             await pause(1); print(".", 1);
             await pause(1); print(".");
+            // sort and join ingredients
             const contents = this.contents.items.map(i => i.name).sort().join(', ')
-            console.log(contents)
+            console.log('mixing', contents)
             if (potions.has(contents)) {
                 // made something
                 this.contents.clear()
-                const potion = potions.get(contents)!();
+                const potion = potions.get(contents)?.();
+                console.log(`created: ${potion.name}`)
                 player.inventory.add(potion);
                 print(`You remove the potion from the pot and wala! it is now: `, 1)
                 color(black)
-                print(`${potion.name}.`)
+                print(`${potion?.name}.`)
             } else {
                 color(black)
                 print("The potion refuses to coalesce.  All ingredients returned.")
@@ -105,17 +198,45 @@ const landmarks: { [key: string]: (...args: any[]) => Landmark } = {
             description: 'corpse of the wise man'
         })
     },
+    large_rock(): Landmark {
+        return new Landmark({
+            name: 'rock',
+            description: 'A Large Rock'
+        })
+    },
+    spire(): Landmark {
+        return new Landmark({
+            name: 'spire',
+            description: "A Towering Sandstone Spire"
+        })
+    },
+    portal_stone(): Landmark {
+        return new Landmark({
+            name: 'portal stone',
+            description: "Eldin's Portal Stone"
+        }).action('transport', async function (player) {
+            color(blue)
+            print("You touch the portal stone...")
+            await pause(1)
+            player.relocate(player.game.find_location("Eldin's Mountain Cottage"))
+        })
+    },
     unknown(n?: number): Landmark {
         return new Landmark({
             name: `Unknown Landscape Item ${n}`,
             description: `Unknown Landscape Item ${n}`
         })
     }
-}
+} as const
 
 type LandmarkKey = keyof typeof landmarks
 function getLandmark(key: LandmarkKey, ...args: any[]) {
-    return landmarks[key](args)
+    if (!landmarks[key]) {
+        throw new Error(`No landmark with key ${key}`)
+    }
+    const newLandmark = landmarks[key](...args)
+    newLandmark.key = key.toString()
+    return newLandmark
 }
 
 export { getLandmark };
