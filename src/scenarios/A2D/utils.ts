9d0db92801cb860d2d0d653d9fb1925b0c75c2ae
jepidoptera <jepidoptera@live.com>
2024-10-30 16:46:38 -0500

    move "play" and "musicc$" to A2/utils.ts

---
+++ src/scenarios/A2D/utils.ts
@@ -0,0 +1,25 @@
+import { qbColors, black, darkwhite } from "./colors.ts";
+import { randomChoice } from "../../game/utils.ts";
+
+function musicc$(n: number) {
+    return Array.from(
+        { length: n },
+        () => {
+            const color1 = randomChoice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+            const color2 = randomChoice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].filter(c => c !== color1))
+            return [color1, color2]
+        }
+    )
+}
+function play(music: number[][]) {
+    for (let i = 0; i < music.length; i++) {
+        color(qbColors[music[i][0]], qbColors[music[i][1]]);
+        // print music note
+        print("♫", 1);
+        // Play music[i]
+    }
+    color(black, darkwhite);
+    return music
+}
+
+export { musicc$, play };
