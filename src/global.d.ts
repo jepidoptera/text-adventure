aa681996d5d3325feb2d418149e918908f99047d
jepidoptera <jepidoptera@live.com>
2024-11-03 10:07:29 -0600

    move to /src

---
+++ src/global.d.ts
@@ -0,0 +1,12 @@
+declare global {
+    function print(text?: string, extend?: any): void;
+    function input(prompt?: string): Promise<string>;
+    function clear(): void;
+    function color(fg: string, bg?: string): void;
+    function optionBox(options: {title: string, options: string[], default_option?: number}): Promise<number>;
+    function getKey(options?: string[]): Promise<string>;
+    function locate(x: number, y?: number)
+    function pause(seconds: number): Promise<void>;
+}
+
+export {};
