4fedc8e04eb4d9aa4ebb547d85fa69840b8e090e
jepidoptera <jepidoptera@live.com>
2024-11-06 13:21:14 -0600

    refactor so NPCs can use spells

--- src/scenarios/A2D/spells.ts
+++ src/scenarios/A2D/spells.ts
@@ -3,69 +3,76 @@ import { A2dCharacter } from './characters.js';
 import { brightred, black, gray, magenta } from './colors.js';
 import { getBuff } from './buffs.js';
 import { WeaponTypes } from '../../game/item.js';
+import { highRandom } from '../../game/utils.js';
 type AttackAction = (this: A2dCharacter, target: Character) => Promise<void>;
 const abilityLevels = ["Novice", "Ameteur", "Competent", "Proficient", "Adept", "Expert", "Master", "Grand Master"]
 const spellPower = 1.0860331325016919
 
 const spells: Record<string, AttackAction> = {
     newbie: async function (target: Character) {
+        if (!checkRequirements.call(this, 'newbie', (2 + this.abilities['newbie']) / 3)) return;
+        color(brightred);
+        if (this.isPlayer) print(`You send a bolt of sputtering newbie magic at ${target.name}.`);
         await damageSpell({
             spellName: 'newbie',
-            magicCost: (this.abilities['newbie'] + 2) / 3,
-            damage: Math.sqrt(Math.random()) * ((this.abilities['newbie'] ** spellPower + 1) / 3) * this.magic_level + Math.random() * 3,
-            accuracy: Math.random() * (this.coordination + this.abilities['newbie'] / 3),
+            damage: highRandom() * (((this.abilities['newbie'] || 1) ** spellPower + 1) / 3) * this.magic_level + Math.random() * 3,
+            accuracy: Math.random() * (this.coordination + (this.abilities['newbie'] || 1) / 3),
             damageType: 'magic',
             weaponType: 'magic',
-            description: "You send a bolt of sputtering newbie magic at"
         }).call(this, target)
     },
     // I want bolt, fire and blades to be relatively on a par with each other.
     // they'll differ more qualitatively than quantitatively.
     bolt: async function (target: Character) {
+        // bolt is low cost, low damage, high accuracy.
+        if (!checkRequirements.call(this, 'bolt', 3 + this.abilities['bolt'])) return;
+        color(brightred);
+        if (this.isPlayer) print(`A jagged flash of lighning strikes towards ${target.name}.`);
         await damageSpell({
             spellName: 'bolt',
-            magicCost: 3 + this.abilities['bolt'],
-            damage: Math.sqrt(Math.random()) * (1.5 + this.abilities['bolt'] ** spellPower * 11 / 14) * this.magic_level + Math.random() * 5,
-            accuracy: Math.random() * (1.5 + this.coordination * (1.5 + this.abilities['bolt'] / 5)),
+            damage: highRandom() * (1.5 + (this.abilities['bolt'] || 1) ** spellPower * 11 / 14) * this.magic_level + Math.random() * 5,
+            accuracy: Math.random() * (1.5 + this.coordination * (1.5 + (this.abilities['bolt'] || 1) / 5)),
             damageType: 'electric',
             weaponType: 'electric',
-            description: "A jagged flash of lighning strikes towards"
         }).call(this, target)
     },
     fire: async function (this: A2dCharacter, target: Character) {
         // fire is medium cost, medium damage, medium accuracy.
+        if (!checkRequirements.call(this, 'fire', 6 + this.abilities['fire'] * 2)) return;
+        color(brightred);
+        if (this.isPlayer) print(`A jet of flame shoots towards ${target.name}.`);
         await damageSpell({
             spellName: 'fire',
-            magicCost: 6 + this.abilities['fire'] * 2,
-            damage: Math.sqrt(Math.random()) * (1 + this.abilities['fire'] ** spellPower) * this.magic_level,
-            accuracy: Math.random() * (this.coordination + 2 + this.abilities['fire'] / 3),
+            damage: highRandom() * (1 + (this.abilities['fire'] || 1) ** spellPower) * this.magic_level,
+            accuracy: Math.random() * (this.coordination + 2 + (this.abilities['fire'] || 1) / 3),
             damageType: 'fire',
             weaponType: 'fire',
-            description: "A jet of flame shoots towards"
         }).call(this, target)
     },
     blades: async function (this: A2dCharacter, target: Character) {
         // blades is high cost, high damage, low accuracy.
+        if (!checkRequirements.call(this, 'blades', 10 + this.abilities['blades'] * 3)) return;
+        color(brightred);
+        if (this.isPlayer) print(`Blades sprout from your fingers and hurtle towards ${target.name}.`);
         await damageSpell({
             spellName: 'blades',
-            magicCost: 10 + this.abilities['blades'] * 3,
-            damage: Math.sqrt(Math.random()) * (1 + this.abilities['blades'] ** spellPower * 1.25) * this.magic_level,
-            accuracy: Math.random() * (Math.max(this.coordination - 1, 1) + this.abilities['blades'] / 5),
+            damage: highRandom() * (1 + (this.abilities['blades'] || 1) ** spellPower * 1.25) * this.magic_level,
+            accuracy: Math.random() * (Math.max(this.coordination - 1, 1) + (this.abilities['blades'] || 1) / 5),
             weaponType: 'blades',
             damageType: 'sharp',
-            description: "Blades sprout from your fingers and hurtle towards"
         }).call(this, target)
     },
     powermaxout: async function (this: A2dCharacter, target: Character) {
         // maximum cost, very high damage, very high accuracy.
+        if (!checkRequirements.call(this, 'powermaxout', Math.max(this.mp, 50))) return;
+        color(brightred);
+        if (this.isPlayer) print(`$A booming wave of ULTIMATE POWER rolls towards ${target.name}.`);
         await damageSpell({
             spellName: 'powermaxout',
-            magicCost: Math.max(this.mp, 50),
-            damage: Math.sqrt(Math.random()) * (this.mp / 25 * this.abilities['powermaxout'] ** spellPower) * this.magic_level,
-            accuracy: Math.sqrt(Math.random()) * 2 * (this.coordination + this.abilities['powermaxout'] * 2),
+            damage: highRandom() * (this.mp / 25 * (this.abilities['powermaxout'] || 1) ** spellPower) * this.magic_level,
+            accuracy: highRandom() * 2 * (this.coordination + (this.abilities['powermaxout'] || 1) * 2),
             weaponType: 'magic',
-            damageType: 'magic',
-            description: "A booming wave of ULTIMATE POWER rolls towards"
+            damageType: 'magic'
         }).call(this, target)
     },
     shield: async function (this: A2dCharacter) {
@@ -119,36 +126,37 @@ const spells: Record<string, AttackAction> = {
     }
 }
 
-function damageSpell({ spellName, magicCost, damage, accuracy, damageType, weaponType, description }: {
+function checkRequirements(this: A2dCharacter, spellName: string, magicCost: number): boolean {
+    if (!this.isPlayer) return true;
+    if (!this.abilities[spellName]) {
+        if (this.isPlayer) print("You don't know that spell.");
+        return false;
+    } else if (this.mp < magicCost) {
+        if (this.isPlayer) print("You don't have enough magic.");
+        return false;
+    } else if (!this.fighting) {
+        if (this.isPlayer) print("There is no target for that spell.");
+        return false;
+    }
+    this.mp -= magicCost;
+    return true;
+}
+
+function damageSpell({ spellName, damage, accuracy, damageType, weaponType }: {
     spellName: string,
-    magicCost: number,
     damage: number,
     accuracy: number,
     weaponType: WeaponTypes,
-    damageType: DamageTypes,
-    description: string
+    damageType: DamageTypes
 }) {
     return async function (this: A2dCharacter, target: Character) {
         // console.log(this.abilities)
-        if (!this.abilities[spellName]) {
-            if (this.isPlayer) print("You don't know that spell.");
-            return;
-        } else if (this.mp < magicCost) {
-            if (this.isPlayer) print("You don't have enough magic.");
-            return;
-        } else if (!this.fighting) {
-            if (this.isPlayer) print("There is no target for that spell.");
-            return;
-        }
-        this.mp -= magicCost;
         const hit = accuracy > target.evasion;
         if (!hit) {
             damage = -1;
         } else {
             damage = Math.max(this.damage_modifier(damage, damageType), 0);
         }
-        color(brightred);
-        print(`${description} ${target.name}.`);
         color(damage ? black : gray)
         print(this.describeAttack(target, spellName, weaponType, damage, false))
         await target.hurt(damage, damageType, this);
