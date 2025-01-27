4f6d159df1116f0d9581b0e9e32829c711e2526f
jepidoptera <jepidoptera@live.com>
2025-01-26 22:12:26 -0600

    musicc returns a string

--- src/scenarios/A2D/utils.ts
+++ src/scenarios/A2D/utils.ts
@@ -1,25 +1,24 @@
-import { qbColors, black, darkwhite } from "../../game/colors.js";
+import { qbColors, black, darkwhite, colorDict } from "../../game/colors.js";
 import { randomChoice } from "../../game/utils.js";
 
 function musicc$(n: number) {
-    return Array.from(
+    let music = Array.from(
         { length: n },
         () => {
-            const color1 = randomChoice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
-            const color2 = randomChoice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].filter(c => c !== color1))
+            const color1 = randomChoice(Object.keys(colorDict))
+            const color2 = randomChoice(Object.keys(colorDict).filter(c => c !== color1))
             return [color1, color2]
         }
     )
-}
-function play(music: number[][]) {
+    let song = '';
     for (let i = 0; i < music.length; i++) {
-        color(qbColors[music[i][0]], qbColors[music[i][1]]);
+        song += `<${music[i][0]}, ${music[i][1]}>`;
         // print music note
-        print("♫", 1);
+        song += "♫";
         // Play music[i]
     }
-    color(black, darkwhite);
-    return music
+    song += `<black, darkwhite> `;
+    return song;
 }
 
-export { musicc$, play };
+export { musicc$ };
